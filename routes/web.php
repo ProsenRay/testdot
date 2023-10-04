58d4964ea9a6fb6c45fe3ac4eaf557cc23b745b9
ProsenRay <prosen7071@gmail.com>
2023-10-03 22:09:22 -1200

    backend templating complete

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('index');
+// });
+
 Route::get('/', function () {
-    return view('index');
+    return view('backend.index');
 });
 
 
